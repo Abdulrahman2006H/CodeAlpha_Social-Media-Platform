c09280ede5c2bbbe96b391c3c6c6fd543d79920b
Abdulrahman2006H <Abdulrahman.Hossam2006@gmail.com>
2026-06-11 22:04:15 +0300

    Add Angular app shell and routing

---
+++ socialmedia(Frontend)/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: '**', redirectTo: 'login' }
+];
