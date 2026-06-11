3b1adcf0ef9408ac4f0733dd55465db7c25decd8
Abdulrahman2006H <Abdulrahman.Hossam2006@gmail.com>
2026-06-11 20:20:05 +0300

    Add authentication API endpoints

---
+++ SocialMedia.Api/SocialMedia.Api/Controllers/AuthController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SocialMedia.Api.Data;
+using SocialMedia.Api.Models;
+
+namespace SocialMedia.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly SocialMediaDbContext _context;
+
+        public AuthController(SocialMediaDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(User user)
+        {
+            var emailExists = await _context.Users.AnyAsync(u => u.Email == user.Email);
+
+            if (emailExists)
+                return BadRequest("Email already exists");
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(new
+            {
+                user.Id,
+                user.FullName,
+                user.UserName,
+                user.Email,
+                user.Bio,
+                user.ProfileImage
+            });
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginRequest request)
+        {
+            var user = await _context.Users
+                .FirstOrDefaultAsync(u => u.Email == request.Email && u.Password == request.Password);
+
+            if (user == null)
+                return Unauthorized("Invalid email or password");
+
+            return Ok(new
+            {
+                user.Id,
+                user.FullName,
+                user.UserName,
+                user.Email,
+                user.Bio,
+                user.ProfileImage
+            });
+        }
+    }
+
+    public class LoginRequest
+    {
+        public string Email { get; set; } = "";
+        public string Password { get; set; } = "";
+    }
+}
